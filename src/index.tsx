ac39036935d61d08cf442db88ae69f5454a8f5bd
nataliasabadysh <nataliasabadysh@gmail.com>
2020-05-28 11:06:59 +0300

    init app

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
+// Core
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// Theme
+import './assets/index.css';
+// App
 import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
